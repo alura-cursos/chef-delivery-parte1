ab72f892eb8e612943e6a5f13d04d8e59bf4796b
Ândriu <andriu_coelho1993@hotmail.com>
2023-05-29 13:18:20 -0300

    aula-1

---
+++ ChefDelivery/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ChefDelivery
+//
+//  Created by ALURA on 17/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Teste")
+            Text("Teste")
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewLayout(.sizeThatFits)
+    }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# chef-delivery-parte1
